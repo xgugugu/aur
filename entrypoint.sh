c5c8374f90bd1efd4eed0afd5f6a56ccefe17ede
咕咕 <xcz2034830996@163.com>
2024-08-26 14:52:28 +0800

    首次提交

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+set -e
+
+pacman -Syu base-devel git --noconfirm
+
+# add user 'buildaur'
+useradd buildaur -m
+echo "buildaur ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+chmod -R a+rw .
+
+# build yay
+git clone https://aur.archlinux.org/yay.git
+(
+    cd yay
+    sudo -H -u buildaur makepkg --syncdeps --install --noconfirm
+)
+
+# add old repo
+if [ -f ./repo.txt ]; then
+    repocfg="\n[custom]\nSigLevel = Optional TrustAll\nServer"
+    repourl=$(cat ./repo.txt)
+    echo "${repocfg} = ${repourl}" >>/etc/pacman.conf
+    pacman -Syu
+fi
+
+mkdir build
+for pkgname in $(cat filename); do
+    sudo -H -u yay -S "${pkgname}" --noconfirm --builddir ./build
+done
+
+ls -lh .
